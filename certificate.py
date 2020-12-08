dc831c87174f9413516e5ec3ee7b6bd735424682
Abhijith Kannan <abhijithkannan452@gmail.com>
2020-12-08 14:33:35 +0530

    Update certificate.py

--- certificate.py
+++ certificate.py
@@ -1,8 +1,5 @@
-""" # Python code to generate certificates from a list of names
-    # Author : ijas <ijas.dev>
-    # Date : 23 June 2020
-"""
-# import os 
+
+import os 
 import pandas as pd
 from PIL import Image, ImageFont, ImageDraw
 
@@ -10,37 +7,29 @@ FONT_FILE = ImageFont.truetype(r'Poppins-Bold.ttf', 130)
 FONT_COLOR = "#000000"
 WIDTH, HEIGHT = 3508, 2480
 
-form = pd.read_excel("Certificate Names list.xlsx")
+form = pd.read_excel("Enter the name of the excel sheet.xlsx")
 name_list = form['Quiz'].to_list()
 
 
 def make_cert(name):
     """function to generate certificate"""
-    image_source = Image.open('Treasure Hunt Certificate.png')
+    image_source = Image.open('Enter the name of the template.png')
     draw = ImageDraw.Draw(image_source)
     name_width, name_height = draw.textsize(name, font=FONT_FILE)
-    draw.text((WIDTH / 2 - name_width / 2, HEIGHT / 2 - name_height / 2 + 20), name, fill=FONT_COLOR, font=FONT_FILE)
+    draw.text((WIDTH / 2 - name_width / 2, HEIGHT / 2 - name_height / 2 ), name, fill=FONT_COLOR, font=FONT_FILE)
     image_source.save("./out/"+str(name)+"/" + name +" 1"+ ".jpg")
     print('printing certificate of: ' + name)
 
 
 
 for i in name_list:
-#    path = "./out/"
-#    os.chdir(path)
-#    newfolder = str(i)
-#    os.mkdir(newfolder)
-#    os.chdir("../")
-   make_cert("Albin Kuruvilla")
+   path = "./out/"
+   os.chdir(path)
+   newfolder = str(i)
+   os.mkdir(newfolder)
+   os.chdir("../")
+   make_cert(i)
 
 
 
-# Content Wrting Certificate
-# Quiz Certificate
-# Resume Building Certificate
-# Treasure Hunt Certificate
 
-# Treasure Hunt	
-# Content	
-# Resume	
-# Quiz
